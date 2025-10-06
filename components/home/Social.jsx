cdbbb29bcb5d5088f62cfd68c60a55bb3a9aef42
Chamod Shyamal <129838930+madusanakcs@users.noreply.github.com>
2025-10-06 20:04:14 +0530

    Update Social.jsx

--- components/home/Social.jsx
+++ components/home/Social.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/__nvk____/" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/chan_da_lee_s/?igsh=MXhycmg2d3pmand2dg%3D%3D" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/kannangaranv" className="home__social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/chandalee-kalubandara-870920316/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="home__social-icon" target="_blank">
             <i className="uil uil-linkedin"></i>
         </a>
 
-        <a href="https://github.com/kannangaranv" className="home__social-icon" target="_blank">
+        <a href="https://github.com/chandaleekalubandara-cs" className="home__social-icon" target="_blank">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
